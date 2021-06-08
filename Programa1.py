0a4212d795671dbb64470c747e8db259ac4a87a3
TereLoza <tere.loza@gmail.com>
2021-06-08 14:05:13 -0500

    agregue archivo saludar

--- Programa1.py
+++ Programa1.py
@@ -1,1 +1,4 @@
+import saludar as s
 print("Hola mundo, soy Tere")
+n = input("Tu nombre es: ")
+s.saludarA(n)
---
+++ saludar.py
@@ -0,0 +1,4 @@
+def saludarA (nombre):
+    print("Saludos",nombre,"\n")
+    print("Bienvenido")
+    
